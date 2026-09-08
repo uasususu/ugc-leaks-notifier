fd355fcb9b8483f4eb2ab2bc44f95848d17e329a
uasususu <c3576062@gmail.com>
2026-09-08 21:27:31 +0300

    Initialize Package.js for ugcleaks-scraper project

---
+++ Package.js
@@ -0,0 +1,15 @@
+{
+  "name": "ugcleaks-scraper",
+  "version": "1.0.0",
+  "description": "Playwright scraper that posts UGC leaks to a Discord webhook",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "author": "",
+  "license": "MIT",
+  "dependencies": {
+    "playwright": "^1.40.0",
+    "undici": "^6.0.0"
+  }
+}
